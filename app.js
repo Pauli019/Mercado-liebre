beced902e18b2daf2b0fb6b9d9b015b200e6d4a9
Pauli019 <paulimonjes@gmail.com>
2022-02-12 00:01:38 -0300

    agregando env.PORT

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen (3000, () => console.log('Servidor corriendo en puerto 3000'));
+
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname,'./views/register.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname,'./views/login.html')));
 app.post('/', (req, res) => res.sendFile(path.resolve(__dirname,'./views/home.html')));
+
+app.listen (process.env.PORT || 3000, function() {
+    console.log('Servidor corriendo en puerto 3000');
+});
